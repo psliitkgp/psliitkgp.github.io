da5d644bc1a3416513c7cb61e66a248ace5624ee
psliitkgp <psllab.phy.iitkgp@gmail.com>
2025-12-01 15:02:03 +0000

    Deploying to gh-pages from @ psliitkgp/psliitkgp.github.io@934e38af4ce27f65b8a6ad645143075fccb5b74a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,16 +419,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-congratulaitons-to-mr-subhajit-dutta-for-his-latest-publication-on-spectral-dynamics-of-a-microbottle-laser-under-thermal-perturbation-in-optical-materials",
+          title: 'Congratulaitons to Mr. Subhajit Dutta for his latest publication on “Spectral dynamics of...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
